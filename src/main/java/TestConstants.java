46f1adca3ecfd0ea4f3941f5c57774143fccff41
vandakiara <vandabarata91@gmail.com>
2020-11-20 10:25:43 +0000

    setup: Initialize structure and basis url

---
+++ src/main/java/TestConstants.java
@@ -0,0 +1,12 @@
+/**
+ * Constants used for the tests.
+ *
+ * @author Vanda Barata (vandabarata91@gmail.com)
+ */
+public class TestConstants {
+
+    /**
+     * The starting url for the selenium tests to be executed.
+     */
+    static final String BASE_URL = "https://shop.mercedes-benz.com/en-gb/collection/";
+}
